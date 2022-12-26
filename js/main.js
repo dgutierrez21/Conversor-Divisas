db280b20e1723922fdead0084f5e692365bb6c3e
Dixon G <dxxn0101@gmail.com>
2022-12-25 22:13:45 -0600

    feat: Creación de la función, envío de la solicitud a la API

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-const selectListaDivisas = document.querySelectorAll(".lista-divisas select");
+const selectListaDivisas = document.querySelectorAll(".lista-divisas select"),
+  btnConvertir = document.querySelector("form button"),
+  deMoneda = document.querySelector(".de select"),
+  aMoneda = document.querySelector(".a select");
 
 for (let i = 0; i < selectListaDivisas.length; i++) {
   for (const codigoDivisa in codigosPaises) {
@@ -11,4 +14,34 @@ for (let i = 0; i < selectListaDivisas.length; i++) {
     let option = `<option value="${codigoDivisa}" ${selected}>${codigoDivisa}</option>`;
     selectListaDivisas[i].insertAdjacentHTML("beforeend", option);
   }
+}
+
+btnConvertir.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  obtenerTipoCambio();
+});
+
+function obtenerTipoCambio() {
+  const cantidad = document.querySelector(".cantidad input");
+  let valorCantidad = cantidad.value;
+
+  if (valorCantidad === "" || valorCantidad === "0") {
+    cantidad.value = "1";
+    valorCantidad = 1;
+  }
+
+  let url = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${deMoneda.value}`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((result) => {
+      let tipocambio = result.conversion_rates[aMoneda.value];
+
+      let tipocambioTotal = (valorCantidad * tipocambio).toFixed(2);
+
+      console.log(result);
+      console.log(tipocambio);
+      console.log(tipocambioTotal);
+    });
 }
